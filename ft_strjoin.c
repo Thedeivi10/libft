5322268463690dbb89bd5f2c5482bdf2d85fa2a9
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-01-27 21:19:37 +0100

    change to ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 13:12:15 by davigome          #+#    #+#             */
-/*   Updated: 2025/01/22 15:05:37 by davigome         ###   ########.fr       */
+/*   Updated: 2025/01/27 21:19:14 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	dst = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!dst)
 		return (0);
-	while (s1[++i])
+	while (s1[++i] && s1)
 		dst[i] = s1[i];
-	while (s2[++j])
+	while (s2[++j] && s2)
 		dst[i + j] = s2[j];
 	dst [i + j] = 0;
 	return (dst);
