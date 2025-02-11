080015382ea21343f5ba819cf6b5560d1014ba1b
Thedeivi10 <d.gomezlanderolopez@gmail.com>
2025-02-11 10:05:07 +0100

    restored ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: davigome <davigome@studen.42malaga.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 20:21:16 by davigome          #+#    #+#             */
-/*   Updated: 2025/02/10 15:38:24 by davigome         ###   ########.fr       */
+/*   Updated: 2025/02/11 10:04:38 by davigome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if (s != NULL)
-	{
-		ft_putstr_fd(s, fd);
-		ft_putchar_fd('\n', fd);
-		return ((int)ft_strlen(s) + 1);
-	}
-	return (0);
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(s))
+		write(fd, &s[i++], 1);
+	write(fd, "\n", 1);
 }
